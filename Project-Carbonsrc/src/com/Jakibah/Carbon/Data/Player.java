fd9939e213e5c9928ac785183ffcb80be455b149
Iakovos <iakovospetrouentertainment@gmail.com>
2016-07-04 22:41:28 +0200

    Changed some unimportant thingies.

--- Project-Carbonsrc/src/com/Jakibah/Carbon/Data/Player.java
+++ Project-Carbonsrc/src/com/Jakibah/Carbon/Data/Player.java
@@ -11,7 +11,6 @@ import com.Jakibah.Carbon.Utilities.Time;
 
 
 public class Player {
-
 	private static String tex;
 	private static float x;
 	private static float y;
@@ -21,19 +20,24 @@ public class Player {
 	private static ArrayList<Player> Players;
 	
 	
-	public Player(String tex, float speed, float x, float y, float width, float height){
+	
+	
+	public Player (String tex, float speed, float x, float y, float width, float height){
+		
 		Player.tex = tex;
 		Player.x = x;
 		Player.y = y;
 		Player.width = width;
 		Player.height = height;
 		Player.speed = speed;
-		this.setPlayers(new ArrayList<Player>());
-		Players.add(this);
+        this.setPlayers(new ArrayList<Player>());
+        Players.add(this);
 		
 		
 	}
-	public static float Direction = 2;
+	
+	
+	public static int Direction = 2;
 	public static boolean Moving = false;
 	public static boolean start = false;
 	
@@ -51,20 +55,20 @@ public static void Walk(){
 	}
 	if(Direction == 0 && Moving == true ){
 		System.out.println("Walking North");
-		  getPlayers().get(0).setY(y -= speed * Time.getDelta());
+		  getPlayers().get(0).setY(y -= speed * Delta());
 		}
 	if(Direction == 1 && Moving == true ){
 		System.out.println("Walking East");
-		getPlayers().get(0).setX(x += speed * Time.getDelta());
+		getPlayers().get(0).setX(x += speed * Delta());
 		
 		}
 	if(Direction == 2 && Moving == true ){
 		System.out.println("Walking South");
-		getPlayers().get(0).setY(y += speed * Time.getDelta());
+		getPlayers().get(0).setY(y += speed * Delta());
 		}
 	if(Direction == 3 && Moving == true ){
 		System.out.println("Walking West");
-		getPlayers().get(0).setX(x -= speed * Time.getDelta());
+		getPlayers().get(0).setX(x -= speed * Delta());
 		}
 	if(Direction == 0 && Moving == false ){
 		System.out.println("Standing North");
@@ -102,7 +106,7 @@ public static void Inputs(){
 		}
 
     public static void draw(){
-    	Draw.DrawQuadTex(Draw.QuickLoadTexture(tex, null), x, y, width, height);
+    	DrawQuadTex(QuickLoad(tex), x, y, width, height);
     }
 
 	
@@ -115,39 +119,30 @@ public static void Inputs(){
 	public String getTex() {
 		return tex;
 	}
-
 	public void setTex(String tex) {
 		Player.tex = tex;
 	}
-
 	public float getX() {
 		return x;
 	}
-
 	public void setX(float x) {
 		Player.x = x;
 	}
-
 	public float getY() {
 		return y;
 	}
-
 	public void setY(float y) {
 		Player.y = y;
 	}
-
 	public float getWidth() {
 		return width;
 	}
-
 	public void setWidth(float width) {
 		Player.width = width;
 	}
-
 	public float getHeight() {
 		return height;
 	}
-
 	public void setHeight(float height) {
 		Player.height = height;
 	}
@@ -155,6 +150,12 @@ public static void Inputs(){
 		return Players;
 	}
 	public void setPlayers(ArrayList<Player> players) {
-		Player.Players = players;
+		Players = players;
 	}
-}
+    
+
+    
+    
+	}
+    
+
